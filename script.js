6a6d6dd398cdbbbbeaccbeb2bee5b80ad809ed39
sol0306 <p_7920@naver.com>
2025-06-05 09:01:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+async function getWord(category) {
+  const response = await fetch(`data/${category}.json`);
+  const words = await response.json();
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const word = words[randomIndex];
+
+  document.getElementById('jp').textContent = word.jp;
+  document.getElementById('pron').textContent = word.pronunciation;
+  document.getElementById('kr').textContent = word.kr;
+
+}
